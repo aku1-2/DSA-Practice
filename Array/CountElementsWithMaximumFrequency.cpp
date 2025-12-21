4ef4544182c0afd5320f9879c011de3a38c7061b
akanksha <168823584+aku1-2@users.noreply.github.com>
2025-12-21 16:06:05 +0530

    Added optimized solution
    Updated comments for clarity and added an optimized solution using unordered_map.

--- Array/CountElementsWithMaximumFrequency.cpp
+++ Array/CountElementsWithMaximumFrequency.cpp
@@ -1,8 +1,10 @@
 //Count Elements with Maximum Frequency in an Array
-/* Brute Force Approach:- We will count the frequency of every element and then compare it with maximum frequency 
-and add all the elements which have maximum frequency.*/
-//Time Complexity: O(N^2) because of nested loops.
-//Space Complexity: O(1) because we are using fixed number of variables.
+
+/* 1. Brute Force Approach:- We will count the frequency of every element and then compare it with the maximum frequency 
+and add all the elements that have the maximum frequency.*/
+//Time Complexity: O(n^2) because of nested loops.
+//Space Complexity: O(1) because we are using a fixed number of variables.
+
 #include <iostream>
 using namespace std;
 class Solution {
@@ -42,7 +44,6 @@ public:
         return result;
     }
 };
-
 int main(){
     Solution sol;
     vector<int> nums = {1, 2, 2, 3, 3, 3, 4};
@@ -50,3 +51,40 @@ int main(){
     cout << "Count of elements with maximum frequency: " << count << endl;
     return 0;
 }
+
+
+//######################################################################################################
+
+
+// 2. Optimized Solution using unordered_map
+//Time complexity: O(n) average
+//Space Complexity: O(n)
+
+ int maxFrequencyElements(vector<int>& nums) {
+     unordered_map<int, int> freq;
+     int result=0;
+     int maxfreq=0;
+     
+     for(int i=0; i<nums.size();i++){
+         int num=nums[i];
+         freq[num]++;
+         if (freq[num] > maxfreq)
+             maxfreq = freq[num];
+     }
+     // Sum up all elements that have the maximum frequency
+        for (auto i = freq.begin(); i!= freq.end(); i++) {
+            if (i->second == maxfreq)
+                result += i->second;
+        }
+
+        return result;
+ }
+
+
+
+
+
+
+
+
+
