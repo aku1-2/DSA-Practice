fdda3aabea0f2b4646a225af3343f4ad08a8b00e
[Aku] <[giriiiakanksha@gmail.com]>
2025-12-21 17:47:42 +0530

    📒Added Optimized code.

--- Array/RotateArrayktimes.cpp
+++ Array/RotateArrayktimes.cpp
@@ -1,7 +1,7 @@
 // Rotate Array k times
 // Brute Force Approach:- We will rotate the array k times by moving the last element to the front required times i.e., by k times.
 //Time Complexity: O(n*k) because outer loop executes k times and inner loop executes n times.
-//Space Complexity: O(1) because we are using fixed number of variables.
+//Space Complexity: O(1) because we are using fixed number of variables(last and loop counters i,j).
 
 #include <iostream>
 #include <vector>
@@ -14,10 +14,11 @@ public:
         k=k%n; // to handle k>n.
        for(int i=0;i<k;i++){
         int last = nums[n-1];
+        //shifting elements to right.
         for(int j= n-1; j > 0; j--){
             nums[j]=nums[j-1];
         }
-        nums[0]=last;
+        nums[0]=last;// add last element to first position
        } 
     }
 };
@@ -32,3 +33,19 @@ int main(){
     }
     return 0;
 }
+
+
+
+
+//##################################################################################################
+// Optimized Solution
+//Time Complexity: O(n) because we are using reverse function which goes through the array once.
+//Space Complexity: O(1) because we are reversing the array in place.
+
+void rotate(vector<int>& nums, int k) {
+        int n= nums.size();
+       k=k%n; // to handle k>n
+       reverse(nums.begin(),nums.end()); // reverse the array
+       reverse(nums.begin(),nums.begin()+k);// reverse first k elements 
+       reverse(nums.begin()+k,nums.end());// reverse remaining elements 
+    }
